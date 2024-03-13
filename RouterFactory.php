1d5d4f2938ace72c816c7b868a5db4b6a73421ac
Miroslav Liška <MiRdACz@users.noreply.github.com>
2024-03-13 15:45:46 +0100

    Add files via upload

---
+++ RouterFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Router;
+use Nette;
+use Nette\Application\Routers\RouteList;
+use Nette\Application\Routers\Route;
+use Nette\Utils\Strings;
+use Nette\Utils\Arrays;
+use Nette\Utils\Validators;
+
+class RouterFactory
+{
+
+    public static function createRouter(): RouteList
+	{
+        $router = new RouteList;
+	$router->addRoute('glamping-mapa','Homepage:glampingMapa');
+        $router->addRoute('admin/<action>','Admin:default');
+        $router->addRoute('user/<action>[/<id>]','User:default');
+        $router->addRoute('web/odhlaseni', 'Homepage:out');                
+        $router->addRoute('web/obnova-hesla', 'Homepage:lostpassword');
+
+        $router->addRoute('travel/<action>','Travel:default');
+	$router->addRoute('glamp/<action>','Glamp:default');
+        $router->addRoute('events/<action>','Events:default');
+        $router->addRoute('marketing/<action>','Marketing:default');
+        $router->addRoute('office/<action>','Office:default');
+        $router->addRoute('grafika/<action>','Grafika:default');
+        
+        $router->addRoute('bprUser', 'Homepage:bprUser');
+        $router->addRoute('[<url>]', 'Homepage:default');
+        $router->addRoute('web/prihlaseni', 'Homepage:sign');
+
+
+		return $router;
+	}
+}
