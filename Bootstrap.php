680014844fcf7d827ac84863924fc48d7ad303b1
Miroslav Liška <MiRdACz@users.noreply.github.com>
2024-03-14 22:03:30 +0100

    Update Bootstrap.php

--- Bootstrap.php
+++ Bootstrap.php
@@ -14,7 +14,7 @@ class Bootstrap
 		$configurator = new Configurator;
 		$appDir = dirname(__DIR__);
 
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+		//$configurator->setDebugMode('secret@23.75.345.200'); // pristup remote IP
         //$configurator->setDebugMode(false); // na web
         $configurator->enableTracy($appDir . '/log');
 
